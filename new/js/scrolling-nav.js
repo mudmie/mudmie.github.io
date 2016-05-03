09737c838fc8f130f3738f0768d97d096bd45002
cchutham <ch.chuthamsatid@gmail.com>
2016-05-02 23:47:33 -0400

    Add initial new web

--- new/js/scrolling-nav.js
+++ new/js/scrolling-nav.js
@@ -17,3 +17,19 @@ $(function() {
         event.preventDefault();
     });
 });
+
+//determine greeting messag
+var findDate = new Date();
+var findHour = findDate.getHours();
+var greetingMessage;
+
+if (findHour > 18) {
+    greetingMessage = "Good Evening!";  
+} else if (findHour > 12) {
+    greetingMessage = "Good Afternoon!";
+} else if (findHour > 0) {
+    greetingMessage = "Good Morning!";
+} else greetingMessage = "Hello!";
+
+var el = document.getElementById('greeting');
+el.innerHTML = greetingMessage; //use textContent or innerHTML?
