26ff51340ca781e9debd44afab4743d88888c8ac
cchutham <ch.chuthamsatid@gmail.com>
2018-04-17 00:14:14 -0400

    add navbar

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,52 @@
 import * as React from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  Collapse,
+  Nav,
+  NavItem,
+  NavLink,
+} from "reactstrap";
 
-export class Header extends React.Component {
+export interface HeaderState {
+  isOpen: boolean;
+}
+
+export class Header extends React.Component<{}, HeaderState> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      isOpen: false,
+    };
+    this.toggle = this.toggle.bind(this);
+  }
+
+  public toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  }
   public render() {
-    return <header>Header</header>;
+    return (
+      <header>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">Mudmie</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/components/">Components</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">
+                  GitHub
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </header>
+    );
   }
 }
