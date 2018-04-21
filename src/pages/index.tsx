8780d8ff6901a51c5bfbcc4229e7f6fa3b41a2fc
cchutham <ch.chuthamsatid@gmail.com>
2018-04-21 16:19:47 -0400

    set up homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { Col, Container, Row, Jumbotron } from "reactstrap";
+import { BackgroundAnimation } from "../components/background-animation";
 
 export interface IndexPageProps {}
 
@@ -7,11 +8,10 @@ export default class IndexPage extends React.Component<IndexPageProps, {}> {
   public render() {
     return (
       <Container fluid>
-        <Row>
+        <Row style={{ position: "relative", minHeight: "500px" }}>
+          <BackgroundAnimation />
           <Col>
-            <Jumbotron>
-              <h1 className="display-3">Hello!</h1>
-            </Jumbotron>
+            <h1 className="display-3">Hello!</h1>
           </Col>
         </Row>
       </Container>
