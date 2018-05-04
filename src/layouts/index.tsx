56b80304a13adf61d32629050cab05ef9d063ba1
cchutham <ch.chuthamsatid@gmail.com>
2018-05-04 15:36:15 -0400

    update favicon

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -30,6 +30,7 @@ export default class DefaultLayout extends React.Component<
             src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"
             defer
           />
+
           <link
             rel="apple-touch-icon"
             sizes="180x180"
@@ -48,7 +49,6 @@ export default class DefaultLayout extends React.Component<
             href="/favicon-16x16.png"
           />
           <link rel="manifest" href="/site.webmanifest" />
-          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
           <meta name="msapplication-TileColor" content="#da532c" />
           <meta name="theme-color" content="#ffffff" />
         </Helmet>
