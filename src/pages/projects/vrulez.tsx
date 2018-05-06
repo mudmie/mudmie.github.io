c317bc5b9ec69abaa0c9b3d1e6c03fe2bd7ae896
cchutham <ch.chuthamsatid@gmail.com>
2018-05-06 12:56:32 -0400

    add link to vrulz

--- src/pages/projects/vrulez.tsx
+++ src/pages/projects/vrulez.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 import { ProjectHeader } from "../../components/project-header";
 import { ProjectPageProps } from "../../models/project-page-props";
 import * as sharedStyles from "./styles.scss";
+import { Col, Container, Row } from "reactstrap";
+import { OutboundLink } from "gatsby-plugin-google-analytics";
 
 export default class VrulezPage extends React.Component<ProjectPageProps, {}> {
   public render() {
@@ -12,6 +14,18 @@ export default class VrulezPage extends React.Component<ProjectPageProps, {}> {
         <ProjectHeader
           project={this.props.data.allProjectsJson.edges[0].node}
         />
+
+        <Row>
+          <Col className="text-center">
+            <OutboundLink
+              href="http://www.vrulez.com"
+              target="_blank"
+              className={`btn ${sharedStyles.btn}`}
+            >
+              Visit the site
+            </OutboundLink>
+          </Col>
+        </Row>
       </div>
     );
   }
