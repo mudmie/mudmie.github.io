c9dedf1d50979214baa0405b548cd4c0008bc1b1
Panuruj Khambanonda <pk@panurujk.com>
2019-09-21 20:25:03 -0700

    Add "Hack The North" project with placeholder assets

---
+++ src/pages/projects/hack-the-north.tsx
@@ -0,0 +1,51 @@
+import * as React from "react";
+import * as sharedStyles from "./styles.module.scss";
+import { Col, Container, Row } from "reactstrap";
+import { graphql, withPrefix } from "gatsby";
+import { ImageBox } from "../../components/image-box";
+import { ProjectLayout } from "../../components/project-layout";
+import { ProjectPageProps } from "../../models/project-page-props";
+
+export default class HackTheNorthPage extends React.Component<
+  ProjectPageProps,
+  {}
+> {
+  public render() {
+    return (
+      <ProjectLayout
+        allProjects={this.props.data.allProjects.edges.map(e => e.node)}
+        currentProject={this.props.data.project.edges[0].node}
+        mainImage={this.props.data.mainImage.childImageSharp.fluid}
+      >
+        <Container>
+          <Row className="justify-content-center">
+            <Col lg="10">
+              <h3>About the Company</h3>
+              <p>Foo Bar</p>
+            </Col>
+          </Row>
+        </Container>
+      </ProjectLayout>
+    );
+  }
+
+  private getImageUrl(filename: string) {
+    return withPrefix(
+      `/images/${this.props.data.project.edges[0].node.imageFolder}/${filename}`
+    );
+  }
+}
+
+export const pageQuery = graphql`
+  query HackTheNorthQuery($path: String!) {
+    allProjects: allProjectsJson {
+      ...ProjectFields
+    }
+    project: allProjectsJson(filter: { url: { eq: $path } }) {
+      ...ProjectFields
+    }
+    mainImage: file(relativePath: { eq: "Hack The North/main.png" }) {
+      ...MainImageSizes
+    }
+  }
+`;
