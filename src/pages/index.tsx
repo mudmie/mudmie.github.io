bd1389ee112cdc1d5fbe00308cb3114bb9ae6c08
Panuruj Khambanonda <pk@panurujk.com>
2020-05-31 23:19:19 -0700

    Add background to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import { Project } from "../models/project";
 import { AboutSection } from "../sections/about-section";
 import { ProjectsSection } from "../sections/projects-section";
 import { ProjectGroup } from "../models/project-group";
+import * as styles from "./index.module.scss";
 
 export interface IndexPageProps {
   data: {
@@ -29,14 +30,16 @@ export default class IndexPage extends React.Component<IndexPageProps, {}> {
         <Helmet titleTemplate="">
           <title>Mudmie Chuthamsatid - Product Designer</title>
         </Helmet>
-        <AboutSection id="about" />
-        <ProjectsSection
-          id="projects"
-          projects={this.props.data.allProjectsJson.edges.map(e => e.node)}
-          projectGroups={this.props.data.allProjectGroupsJson.edges.map(
-            e => e.node
-          )}
-        />
+        <div className={styles.homePage}>
+          <AboutSection id="about" />
+          <ProjectsSection
+            id="projects"
+            projects={this.props.data.allProjectsJson.edges.map(e => e.node)}
+            projectGroups={this.props.data.allProjectGroupsJson.edges.map(
+              e => e.node
+            )}
+          />
+        </div>
       </Layout>
     );
   }
