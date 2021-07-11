69218cdebe6648673be1f834c08b2a526d7c8a23
Mudmie Chuthams <48304307+mudmie@users.noreply.github.com>
2021-07-11 16:23:07 -0700

    Update footer to include message

--- src/components/project-footer.tsx
+++ src/components/project-footer.tsx
@@ -3,6 +3,8 @@ import { Project } from "../models/project";
 import { Col, Container, Row } from "reactstrap";
 import { ProjectNav } from "./project-nav";
 import { MoreProjects } from "./more-projects";
+import { PROJECT_BODY_COLUMN } from "./project-layout";
+import * as styles from "./project-footer.module.scss";
 
 export interface ProjectFooterProps {
   allProjects: Project[];
@@ -11,13 +13,20 @@ export interface ProjectFooterProps {
 export class ProjectFooter extends React.Component<ProjectFooterProps, {}> {
   public render() {
     return (
-      <div>
+      <>
         {/* <ProjectNav
           projects={this.props.allProjects}
           currentProject={this.props.currentProject}
         /> */}
+        <Container>
+          <Row className="justify-content-center mt-3">
+            <Col lg={PROJECT_BODY_COLUMN} className={styles.horizontalLine}>
+              Congrats! You’ve reached the end of this case study.
+            </Col>
+          </Row>
+        </Container>
         <MoreProjects currentProject={this.props.currentProject} projects={this.props.allProjects} />
-      </div>
+      </>
     );
   }
 }
