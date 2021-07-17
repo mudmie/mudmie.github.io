052665ef167800ab09262c62a8b4d8892c948e96
Panuruj Khambanonda <pk@panurujk.com>
2021-07-16 23:59:34 -0700

    Middle nav bar

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,6 +9,7 @@ import {
   Navbar,
   NavbarToggler,
   NavbarBrand,
+  Container,
 } from "reactstrap";
 import * as styles from "./header.module.scss";
 
@@ -69,79 +70,86 @@ export class Header extends React.Component<HeaderProps, HeaderState> {
     }
     return (
       <header>
-        <Navbar
-          className={`${styles.navbar} ${
-            !this.state.isTop ? styles.scroll : ""
-          } ${this.state.isOpen ? styles.navOpen : ""}`}
-          light={!this.props.isDarkMode}
-          dark={this.props.isDarkMode}
-          expand="md"
-          fixed="top"
-        >
-          <Link
-            to="/"
-            className="navbar-brand"
-            onClick={() => {
-              this.collapseNav();
-            }}
+        <Container>
+          <Navbar
+            className={`${styles.navbar} ${
+              !this.state.isTop ? styles.scroll : ""
+            } ${this.state.isOpen ? styles.navOpen : ""}`}
+            light={!this.props.isDarkMode}
+            dark={this.props.isDarkMode}
+            expand="md"
           >
-            <img src={withPrefix("/images/logo.svg")} className={styles.logo} />
-          </Link>
-          <NavbarToggler className={styles.navbarToggler} onClick={this.toggle}>
-            {togglerButtonText}
-          </NavbarToggler>
+            <Link
+              to="/"
+              className="navbar-brand"
+              onClick={() => {
+                this.collapseNav();
+              }}
+            >
+              <img
+                src={withPrefix("/images/logo.svg")}
+                className={styles.logo}
+              />
+            </Link>
+            <NavbarToggler
+              className={styles.navbarToggler}
+              onClick={this.toggle}
+            >
+              {togglerButtonText}
+            </NavbarToggler>
 
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                <Link
-                  className={`nav-link ${styles.navLink}`}
-                  to="/"
-                  onClick={() => {
-                    this.collapseNav();
-                  }}
-                >
-                  Home
-                </Link>
-              </NavItem>
-              <NavItem>
-                <Link
-                  className={`nav-link ${styles.navLink}`}
-                  to="/#projects"
-                  onClick={() => {
-                    this.collapseNav();
-                  }}
-                >
-                  Projects
-                </Link>
-              </NavItem>
-              <NavItem>
-                <Link
-                  className={`nav-link ${styles.navLink}`}
-                  to="/about"
-                  onClick={() => {
-                    this.collapseNav();
-                  }}
-                >
-                  About
-                </Link>
-              </NavItem>
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <Link
+                    className={`nav-link ${styles.navLink}`}
+                    to="/"
+                    onClick={() => {
+                      this.collapseNav();
+                    }}
+                  >
+                    Home
+                  </Link>
+                </NavItem>
+                <NavItem>
+                  <Link
+                    className={`nav-link ${styles.navLink}`}
+                    to="/#projects"
+                    onClick={() => {
+                      this.collapseNav();
+                    }}
+                  >
+                    Projects
+                  </Link>
+                </NavItem>
+                <NavItem>
+                  <Link
+                    className={`nav-link ${styles.navLink}`}
+                    to="/about"
+                    onClick={() => {
+                      this.collapseNav();
+                    }}
+                  >
+                    About
+                  </Link>
+                </NavItem>
 
-              <NavItem>
-                <OutboundLink
-                  href={withPrefix("/mudmie-resume2021.pdf")}
-                  className={`nav-link ${styles.navLink}`}
-                  target="_blank"
-                  onClick={() => {
-                    this.collapseNav();
-                  }}
-                >
-                  Résumé
-                </OutboundLink>
-              </NavItem>
-            </Nav>
-          </Collapse>
-        </Navbar>
+                <NavItem>
+                  <OutboundLink
+                    href={withPrefix("/mudmie-resume2021.pdf")}
+                    className={`nav-link ${styles.navLink}`}
+                    target="_blank"
+                    onClick={() => {
+                      this.collapseNav();
+                    }}
+                  >
+                    Résumé
+                  </OutboundLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Navbar>
+        </Container>
       </header>
     );
   }
