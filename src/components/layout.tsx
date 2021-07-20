73347c55bb72815e8dc961f53d2423b9a3aff08d
Mudmie Chuthams <48304307+mudmie@users.noreply.github.com>
2021-07-20 15:00:09 -0700

    Revert "try to fix favicon"
    This reverts commit b50dd20e62e8bab7259125fb165bc3919d67721a.

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -28,19 +28,17 @@ export default class Layout extends React.Component<LayoutProps, {}> {
             sizes="180x180"
             href="/apple-touch-icon.png"
           />
-
-
           <link
             rel="icon"
-            type="image/x-icon"
+            type="image/png"
             sizes="32x32"
-            href="/favicon-32x32.ico"
+            href="/favicon-32x32.png"
           />
           <link
             rel="icon"
-            type="image/x-icon"
+            type="image/png"
             sizes="16x16"
-            href="/favicon-16x16.ico"
+            href="/favicon-16x16.png"
           />
           <link rel="manifest" href="/site.webmanifest" />
           <meta name="msapplication-TileColor" content="#da532c" />
