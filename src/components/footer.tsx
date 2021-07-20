03490abbbbfeea62345896aaf33a83d582cfbd83
Panuruj Khambanonda <pk@panurujk.com>
2021-07-19 21:31:06 -0700

    Footer with social

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,15 +2,17 @@ import * as React from "react";
 import { FaRegSmile } from "react-icons/fa";
 import { Col, Container, Row } from "reactstrap";
 import * as styles from "./footer.module.scss";
+import { SocialMediaLinks } from "./social-media-links";
 
 export class Footer extends React.Component {
   public render() {
     return (
-      <footer>
+      <footer className={styles.section}>
         <Container className={styles.container}>
           <Row className={styles.footerRow}>
-            <Col lg="12" className={styles.footer}>
-              Designed &amp; partially coded by Mudmie 2021 ©
+            <Col lg="12" className={styles.footerCol}>
+              Designed &amp; partially coded by Mudmie 2021 ©<br />
+              <SocialMediaLinks separator=" " />
             </Col>
           </Row>
         </Container>
