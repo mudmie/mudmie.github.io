b50dd20e62e8bab7259125fb165bc3919d67721a
Mudmie Chuthams <48304307+mudmie@users.noreply.github.com>
2021-07-20 14:52:05 -0700

    try to fix favicon

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -28,17 +28,19 @@ export default class Layout extends React.Component<LayoutProps, {}> {
             sizes="180x180"
             href="/apple-touch-icon.png"
           />
+
+
           <link
             rel="icon"
-            type="image/png"
+            type="image/x-icon"
             sizes="32x32"
-            href="/favicon-32x32.png"
+            href="/favicon-32x32.ico"
           />
           <link
             rel="icon"
-            type="image/png"
+            type="image/x-icon"
             sizes="16x16"
-            href="/favicon-16x16.png"
+            href="/favicon-16x16.ico"
           />
           <link rel="manifest" href="/site.webmanifest" />
           <meta name="msapplication-TileColor" content="#da532c" />
