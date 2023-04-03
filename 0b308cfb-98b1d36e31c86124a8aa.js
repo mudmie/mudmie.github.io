84ca0ec31b27f9f90b4735e4ff1ae83a2593bdf9
Mudmie Chuthams <48304307+mudmie@users.noreply.github.com>
2023-04-03 00:41:01 -0700

    Updates

---
+++ 0b308cfb-98b1d36e31c86124a8aa.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkMudmie_com=self.webpackChunkMudmie_com||[]).push([[827],{9274:function(e,t,u){u.d(t,{iq:function(){return i}});var a=u(4405);function i(e){return(0,a.w_)({tag:"svg",attr:{viewBox:"0 0 24 24",fill:"currentColor","aria-hidden":"true"},child:[{tag:"path",attr:{fillRule:"evenodd",d:"M8.25 3.75H19.5a.75.75 0 01.75.75v11.25a.75.75 0 01-1.5 0V6.31L5.03 20.03a.75.75 0 01-1.06-1.06L17.69 5.25H8.25a.75.75 0 010-1.5z",clipRule:"evenodd"}}]})(e)}}}]);
+//# sourceMappingURL=0b308cfb-98b1d36e31c86124a8aa.js.map
